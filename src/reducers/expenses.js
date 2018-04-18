42e811fd251a6a44e3ced6de6edcf5517ad84e1e
Misael Burboa <caleb.burboa@internetbrands.com>
2018-04-17 21:52:11 -0700

    conect redux with react and HOC (higher order component) pattern

---
+++ src/reducers/expenses.js
@@ -0,0 +1,30 @@
+//Expenses Reducer
+const expensesReducerDefaultState = [];
+/**
+*state: current state of store
+*action: new values that comes from the action generators
+*/
+export default (state = expensesReducerDefaultState, action) => {
+    switch(action.type){
+        case 'ADD_EXPENSE':
+            return [
+                ...state, //spreading operator
+                action.expense
+            ];
+        case 'REMOVE_EXPENSE':
+            return state.filter(({ id }) => id !== action.id )
+        case 'EDIT_EXPENSE':
+            return state.map((expense) => {
+                if(expense.id === action.id){
+                    return {
+                        ...expense, //spreading object operator
+                        ...action.updates
+                    }
+                }else{
+                    return expense;
+                }
+            });
+        default:
+            return state;
+    }
+};
