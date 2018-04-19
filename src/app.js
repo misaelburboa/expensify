2c1e17aaccd9f98ea7a7a96bcdb67226ceee257a
Misael Burboa <caleb.burboa@internetbrands.com>
2018-04-19 09:14:27 -0700

    Connecting Redux with React, add filters and remove expense button

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { addExpense, removeExpense, editExpense } from './actions/expenses';
@@ -10,13 +11,22 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water Bill', amount: 100, createdAt: -9999 }));
+store.dispatch(addExpense({ description: 'Water Bill', amount: 4500, createdAt: -9999 }));
 store.dispatch(addExpense({ description: 'Gas Bill', amount: 100, createdAt: -9999 }));
 store.dispatch(addExpense({ description: 'Other Expense', amount: 100, createdAt: -9999 }));
 store.dispatch(setTextFilter("water"));
+
+setTimeout(() => {
+    store.dispatch(setTextFilter("bill"));
+},3000);
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 console.log(visibleExpenses);
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+)
+ReactDOM.render(jsx, document.getElementById('app'));
