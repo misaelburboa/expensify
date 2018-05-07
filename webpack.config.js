5fcc226fa44d421af57619aded59c3013f5f7212
Misael Burboa <caleb.burboa@internetbrands.com>
2018-05-06 18:53:13 -0700

    set environment for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,53 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                //'loader' is used for using a single loader
-                loader: 'babel-loader',
-                test: /\.js$/,
-                exclude: /node_modules/
-            },{
-                "test": /\.s?css$/,
-                //'use' is used for using multiple loaders
-                "use": [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
-            }
-        ]
-    },
-    devtool: 'cheap-module-eval-source-map', //shows the error queue easily for developer, it is used to track the error
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
+module.exports = (env) => {
+    const isProduction =  env === 'production';
+    const CSSExtract = new ExtractTextPlugin('styles.css');
+
+    return{
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [
+                {
+                    //'loader' is used for using a single loader
+                    loader: 'babel-loader',
+                    test: /\.js$/,
+                    exclude: /node_modules/
+                },{
+                    "test": /\.s?css$/,
+                    //'use' is used for using multiple loaders, in this case we are
+                    //using the ExtractTextPlugin in order to get the css files in another separated file
+                    "use": CSSExtract.extract({
+                        use: [
+                            {
+                                loader: 'css-loader',
+                                options: {
+                                    sourceMap: true
+                                }
+                            },
+                            {
+                                loader: 'sass-loader',
+                                options: {
+                                    sourceMap: true
+                                }
+                            }
+                        ]
+                    })
+                }
+            ]
+        },
+        plugins: [
+            CSSExtract
+        ],
+        devtool: isProduction ? 'source-map' : 'inline-source-map', //shows the error queue easily for developer, it is used to track the error | (sourcemaps)
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true
+        }
     }
-};
+}
