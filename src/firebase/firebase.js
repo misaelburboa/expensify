417cee87598cbdc96292f024635b856327adcab9
Misael Burboa <caleb.burboa@internetbrands.com>
2018-05-18 09:21:21 -0700

    fetching data from firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -13,19 +13,57 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 
-database.ref().set({
-    name: 'Misael Burboa',
-    age: 29,
-    isSingle: false,
-    location: {
-        city: 'Hermosillo',
-        country: 'Mexico'
-    }
-}).then(() => {
-    console.log('Data is saved');
-}).catch((e) => {
-    console.log('This failed', e);
+const constOnValueChange = database.ref().on('value', (snapshot) => {
+    const obj = snapshot.val();
+    console.log(`${obj.name} is a ${obj.job.title} in ${obj.job.company}`);
+}, (e) => {
+    console.log("Error");
 });
+
+database.ref('job/company').set('Internet Brands');
+// constOnValueChange = database.ref().on('value', (snapshot) => {
+//     console.log(snapshot.val());
+// }, (e) => {
+//     console.log('Error with data fetching', e);
+// });
+
+
+// setTimeout(() => {
+//     database.ref('age').set(28);
+// }, 3500);
+
+// setTimeout(() => {
+//     database.ref().off(constOnValueChange);
+// }, 7000);
+
+// setTimeout(() => {
+//     database.ref('age').set(31);
+// }, 10500);
+
+// database.ref().set({
+//     name: 'Misael Burboa',
+//     age: 29,
+//     stressLevel: 6,
+//     job: {
+//         title: 'Web Developer',
+//         company: 'Internet Brands'
+//     },
+//     location: {
+//         city: 'Hermosillo',
+//         country: 'Mexico'
+//     }
+// }).then(() => {
+//     console.log('Data is saved');
+// }).catch((e) => {
+//     console.log('This failed', e);
+// });
+
+// database.ref().update({
+//     stressLevel: 9,
+//     'job/company': 'Google',
+//     'location/city': 'LA',
+//     'location/country': 'United States'
+// });
 
 // database.ref('isSingle')
 //     .remove()
