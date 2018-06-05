a69e1621943c602e763498906a9d1d0153102b7b
Misael Burboa <caleb.burboa@internetbrands.com>
2018-06-05 16:04:27 -0700

    testing async redux actions

--- src/playground/promises.js
+++ src/playground/promises.js
@@ -1,16 +1,24 @@
 const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
-        // resolve({
-        //     name: 'Misael',
-        //     age: 29
-        // });
-        reject('Something went wrong!')
+        resolve({
+            name: 'Misael',
+            age: 29
+        });
+        // reject('Something went wrong!')
     }, 1500);
 });
 
 console.log("before");
 promise.then((data) => {
     console.log('1', data);
+    return  new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('This is another promess');
+            // reject('Something went wrong!')
+        }, 1500);
+    });
+}).then((str) => {
+    console.log("does this run?", str);
 }).catch((error) => {
     console.log('error: ', error);
 });
