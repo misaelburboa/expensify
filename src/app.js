93905ba65a14e6756240d20723c2572703c63289
Misael Burboa <caleb.burboa@internetbrands.com>
2018-06-07 10:32:15 -0700

    dispatch is undefined

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense, removeExpense, editExpense } from './actions/expenses';
+import { startSetExpenses } from './actions/expenses';
 import { setTextFilter, sortByAmount, sortByDate, setStartDate, setEndDate } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
@@ -13,18 +13,13 @@ import './firebase/firebase';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water Bill', amount: 4500, createdAt: -9997 }));
-store.dispatch(addExpense({ description: 'Gas Bill', amount: 100, createdAt: -9995 }));
-store.dispatch(addExpense({ description: 'Other Expense', amount: 100, createdAt: -9998 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 109500, createdAt: -9996 }));
-
-// const state = store.getState();
-// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-// console.log(visibleExpenses);
-
 const jsx = (
     <Provider store={store}>
         <AppRouter />
     </Provider>
 )
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<p>Loading</p>, document.getElementById('app'));
+
+store.dispatch(startSetExpenses()).then(() => {
+    ReactDOM.render(jsx, document.getElementById('app'));
+});
