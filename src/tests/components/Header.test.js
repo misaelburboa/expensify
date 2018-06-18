6181d242ea28e40bb4393e0a9646d92839767da0
Misael Burboa <caleb.burboa@internetbrands.com>
2018-06-18 09:57:14 -0700

    Logout action

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,17 +1,15 @@
-import ReactShallowRenderer from 'react-test-renderer/shallow';
 import { shallow } from 'enzyme';
 import React from 'react';
-import Header from '../../components/Header';
+import { Header } from '../../components/Header';
 
 test('should render header correctly', () => {
-    const wrapper = shallow(<Header />);
+    const wrapper = shallow(<Header startLogout={() => {}} />);
     expect(wrapper).toMatchSnapshot();
+});
 
-    //Enzyme:
-    //expect(wrapper.find('h1').text()).toBe("Expensify");
-
-    //React Test Renderer:
-    // const renderer = new ReactShallowRenderer();
-    // renderer.render(<Header />);
-    // expect(renderer.getRenderOutput()).toMatchSnapshot();
-})
+test('should call startLogout on button click', () => {
+    const startLogout = jest.fn();
+    const wrapper = shallow(<Header startLogout={startLogout}/> )
+    wrapper.find('button').simulate('click');
+    expect(startLogout).toHaveBeenCalled();
+});
